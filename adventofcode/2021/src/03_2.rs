5d93599217170852e7d757df4847e78dc591d4bb
rhysd <lin90162@yahoo.co.jp>
2021-12-03 22:03:15 +0900

    aoc: reduce allocs

--- adventofcode/2021/src/03_2.rs
+++ adventofcode/2021/src/03_2.rs
@@ -1,27 +1,27 @@
 use std::collections::HashSet;
 use std::io::{self, BufRead};
+use std::str;
 
-fn calc(mut h: HashSet<Vec<char>>, cmp: fn(usize, usize) -> bool) -> usize {
+fn calc(mut h: HashSet<Vec<u8>>, cmp: fn(usize, usize) -> bool) -> usize {
     let mut idx = 0;
     loop {
-        let ones = h.iter().filter(|b| b[idx] == '1').count();
-        let survive = if cmp(ones, h.len() - ones) { '1' } else { '0' };
+        let ones = h.iter().filter(|b| b[idx] == b'1').count();
+        let survive = if cmp(ones, h.len() - ones) { b'1' } else { b'0' };
         h.retain(|x| x[idx] == survive);
         if h.len() == 1 {
             let v = h.into_iter().next().unwrap();
-            return usize::from_str_radix(&v.into_iter().collect::<String>(), 2).unwrap();
+            return usize::from_str_radix(str::from_utf8(&v).unwrap(), 2).unwrap();
         }
         idx += 1;
     }
 }
 
 fn main() {
-    let mut h = HashSet::new();
-    for l in io::BufReader::new(io::stdin().lock()).lines() {
-        h.insert(l.unwrap().chars().collect::<Vec<_>>());
-    }
-
-    let oxy = calc(h.clone(), |x, y| x >= y);
-    let co2 = calc(h, |x, y| x < y);
+    let h = io::BufReader::new(io::stdin().lock())
+        .lines()
+        .map(|l| l.unwrap().into_bytes())
+        .collect::<HashSet<_>>();
+    let oxy = calc(h.clone(), |ones, zeros| ones >= zeros);
+    let co2 = calc(h, |ones, zeros| ones < zeros);
     println!("{}", oxy * co2);
 }
