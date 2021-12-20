6343837e363a58d0fe1587449baece75cfc66217
rhysd <lin90162@yahoo.co.jp>
2021-12-21 01:18:41 +0900

    aoc: suppress unused warning from day 20

--- adventofcode/2021/20.rs
+++ adventofcode/2021/20.rs
@@ -3,42 +3,53 @@ use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum C {
-    L, // light
-    D, // dark
+enum Dot {
+    Light,
+    Dark,
 }
 
-impl C {
-    fn new(c: char) -> C {
+impl From<char> for Dot {
+    fn from(c: char) -> Self {
         match c {
-            '#' => C::L,
-            '.' => C::D,
+            '#' => Dot::Light,
+            '.' => Dot::Dark,
             c => panic!("invalid char {:?}", c),
         }
     }
+}
 
+impl From<Dot> for char {
+    fn from(c: Dot) -> char {
+        match c {
+            Dot::Light => '#',
+            Dot::Dark => '.',
+        }
+    }
+}
+
+impl Dot {
     fn bit(self) -> usize {
         match self {
-            C::L => 1,
-            C::D => 0,
+            Dot::Light => 1,
+            Dot::Dark => 0,
         }
     }
 }
 
 struct Image {
-    enhancements: Box<[C]>,
+    table: Box<[Dot]>,
     min_x: i32,
     min_y: i32,
     max_x: i32,
     max_y: i32,
-    surround: C,
-    current: HashMap<(i32, i32), C>,
+    surround: Dot,
+    dots: HashMap<(i32, i32), Dot>,
 }
 
 impl Image {
-    fn new(mut lines: impl Iterator<Item = String>) -> Self {
-        let enhancements: Vec<_> = lines.next().unwrap().chars().map(C::new).collect();
-        assert_eq!(enhancements.len(), 512);
+    fn parse(mut lines: impl Iterator<Item = String>) -> Self {
+        let table: Vec<_> = lines.next().unwrap().chars().map(Dot::from).collect();
+        assert_eq!(table.len(), 512);
 
         lines.next().unwrap();
 
@@ -46,7 +57,7 @@ impl Image {
         let mut min_x: i32 = i32::MAX;
         let mut max_y: i32 = i32::MIN;
         let mut min_y: i32 = i32::MAX;
-        let mut current = HashMap::new();
+        let mut dots = HashMap::new();
         for (y, line) in lines.enumerate() {
             let y = y as i32;
             for (x, c) in line.chars().enumerate() {
@@ -55,22 +66,22 @@ impl Image {
                 max_y = cmp::max(max_y, y);
                 min_x = cmp::min(min_x, x);
                 min_y = cmp::min(min_y, y);
-                current.insert((x, y), C::new(c));
+                dots.insert((x, y), Dot::from(c));
             }
         }
 
         Self {
-            enhancements: enhancements.into_boxed_slice(),
+            table: table.into_boxed_slice(),
             min_x,
             min_y,
             max_x,
             max_y,
-            current,
-            surround: C::D,
+            dots,
+            surround: Dot::Dark,
         }
     }
 
-    fn enhance_at(&self, x: i32, y: i32) -> C {
+    fn enhance_at(&self, x: i32, y: i32) -> Dot {
         let mut idx = 0usize;
         for bit in [
             (x - 1, y - 1),
@@ -84,19 +95,18 @@ impl Image {
             (x + 1, y + 1),
         ]
         .into_iter()
-        .map(|p| self.current.get(&p).copied().unwrap_or(self.surround).bit())
+        .map(|p| self.dots.get(&p).copied().unwrap_or(self.surround).bit())
         {
             idx = (idx << 1) + bit
         }
-        self.enhancements[idx]
+        self.table[idx]
     }
 
     fn enhance(&mut self) {
         let mut next = HashMap::new();
-        for (x, y) in self.current.keys() {
+        for (x, y) in self.dots.keys() {
             let (x, y) = (*x, *y);
-            let c = self.enhance_at(x, y);
-            next.insert((x, y), c);
+            next.insert((x, y), self.enhance_at(x, y));
         }
 
         self.min_x -= 1;
@@ -114,27 +124,24 @@ impl Image {
             }
         }
 
-        self.current = next;
+        self.dots = next;
 
         self.surround = match self.surround {
-            C::D => self.enhancements[0],
-            C::L => self.enhancements[0x1ff],
+            Dot::Dark => self.table[0],
+            Dot::Light => self.table[0x1ff],
         };
     }
 
     fn count_lits(&self) -> usize {
-        assert_eq!(self.surround, C::D);
-        self.current.iter().filter(|(_, c)| **c == C::L).count()
+        assert_eq!(self.surround, Dot::Dark);
+        self.dots.iter().filter(|(_, c)| **c == Dot::Light).count()
     }
 
+    #[allow(dead_code)]
     fn print(&self) {
         for y in self.min_y..=self.max_y {
             for x in self.min_x..=self.max_x {
-                let c = match *self.current.get(&(x, y)).unwrap() {
-                    C::L => '#',
-                    C::D => '.',
-                };
-                print!("{}", c);
+                print!("{}", char::from(*self.dots.get(&(x, y)).unwrap()));
             }
             println!();
         }
@@ -142,7 +149,7 @@ impl Image {
 }
 
 fn main() {
-    let mut image = Image::new(io::stdin().lock().lines().map(Result::unwrap));
+    let mut image = Image::parse(io::stdin().lock().lines().map(Result::unwrap));
     for _ in 0..2 {
         image.enhance();
     }
