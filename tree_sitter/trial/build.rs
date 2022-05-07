9d62a0ea514dfec6b6c9b088ad4babb5755b31ee
rhysd <lin90162@yahoo.co.jp>
2022-05-08 00:08:11 +0900

    tree_sitter: rebuild when parser sources are changed

--- tree_sitter/trial/build.rs
+++ tree_sitter/trial/build.rs
@@ -1,11 +1,15 @@
 use std::path::Path;
 
 fn main() {
-    let src = Path::new("tree-sitter-rust").join("src");
+    let src_dir = Path::new("tree-sitter-rust").join("src");
+    let sources = &[src_dir.join("parser.c"), src_dir.join("scanner.c")];
 
     cc::Build::new()
-        .file(src.join("parser.c"))
-        .file(src.join("scanner.c"))
-        .include(src.join("tree_sitter"))
+        .files(sources)
+        .include(src_dir.join("tree_sitter"))
         .compile("tree-sitter-rust");
+
+    for src in sources {
+        println!("cargo:rerun-if-changed={}", src.to_str().unwrap());
+    }
 }
