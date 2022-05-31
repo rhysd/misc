8767316e34c6eb60f53066aa256e21937688063d
rhysd <lin90162@yahoo.co.jp>
2022-05-31 22:29:42 +0900

    tree_sitter: add terminal UI to calc

--- tree_sitter/calc/src/main.rs
+++ tree_sitter/calc/src/main.rs
@@ -1,9 +1,23 @@
 use anyhow::{bail, Context, Result};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use std::env;
 use std::fs;
 use std::io;
 use std::io::Read;
-use tree_sitter::{Language, Node, Parser};
+use tree_sitter::{InputEdit, Language, Node, Parser, Tree};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
 
 fn read_input(path: Option<&str>) -> Result<String> {
     if let Some(path) = path {
@@ -164,6 +178,7 @@ impl<'a> Interpreter<'a> {
     }
 }
 
+/*
 fn main() -> Result<()> {
     let file = env::args().skip(1).next();
     let file = file.as_ref().map(String::as_str);
@@ -181,4 +196,191 @@ fn main() -> Result<()> {
     let ret = interpreter.eval(&tree.root_node())?;
     println!("{}", ret);
     Ok(())
+}
+*/
+
+enum Edit {
+    Char(char),
+    Del,
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    source: String,
+    sexp: String,
+    parser: Parser,
+    tree: Option<Tree>,
+}
+
+impl App {
+    fn new() -> Result<Self> {
+        let lang = tree_sitter_calc::language();
+        let mut parser = Parser::new();
+        parser.set_language(lang)?;
+        Ok(App {
+            source: String::new(),
+            sexp: String::new(),
+            parser,
+            tree: None,
+        })
+    }
+
+    fn update_sexp(&mut self) {
+        // TODO: When no change, just return
+        self.sexp = if let Some(tree) = &self.tree {
+            tree.root_node().to_sexp()
+        } else {
+            "Could not parse input (Parser::parse returned None)".to_string()
+        };
+    }
+
+    fn reparse(&mut self) {
+        // Grammar always requires \n at end of statement
+        self.source.push('\n');
+        self.tree = self.parser.parse(&self.source, self.tree.as_ref());
+        self.source.pop(); // pop \n
+    }
+
+    fn calc(&mut self, edit: Edit) {
+        match edit {
+            Edit::Char(c) => {
+                if let Some(tree) = &mut self.tree {
+                    let node = tree.root_node();
+                    let old_pos = node.end_position();
+                    let new_pos = {
+                        let mut p = old_pos;
+                        p.column += 1;
+                        p
+                    };
+                    let old = self.source.len();
+                    let new = old + c.len_utf8();
+                    let edit = InputEdit {
+                        start_byte: old,
+                        old_end_byte: old,
+                        new_end_byte: new,
+                        start_position: old_pos,
+                        old_end_position: old_pos,
+                        new_end_position: new_pos,
+                    };
+                    tree.edit(&edit);
+                }
+                self.source.push(c);
+                self.reparse();
+                self.update_sexp();
+            }
+            Edit::Del => {
+                if let Some(c) = self.source.pop() {
+                    if let Some(tree) = &mut self.tree {
+                        let node = tree.root_node();
+                        let old_pos = node.end_position();
+                        let new_pos = {
+                            let mut p = old_pos;
+                            p.column = p.column.saturating_sub(1);
+                            p
+                        };
+                        let new = self.source.len();
+                        let old = new + c.len_utf8();
+                        let edit = InputEdit {
+                            start_byte: old,
+                            old_end_byte: old,
+                            new_end_byte: new,
+                            start_position: old_pos,
+                            old_end_position: old_pos,
+                            new_end_position: new_pos,
+                        };
+                        tree.edit(&edit);
+                    }
+                    self.reparse();
+                    self.update_sexp();
+                }
+            }
+        }
+    }
+
+    fn render<B: Backend>(&self, f: &mut Frame<B>) {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(2)
+            .constraints(
+                [
+                    Constraint::Length(1),
+                    Constraint::Length(3),
+                    Constraint::Min(1),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+
+        let msg = vec![
+            Span::raw("Press "),
+            Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+            Span::raw(" to exit"),
+        ];
+        let mut text = Text::from(Spans::from(msg));
+        let help_message = Paragraph::new(text);
+        f.render_widget(help_message, layout[0]);
+
+        let input = Paragraph::new(self.source.as_ref())
+            .block(Block::default().borders(Borders::ALL).title("Input"));
+        f.render_widget(input, layout[1]);
+        // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
+        f.set_cursor(
+            // Put cursor past the end of the input text
+            layout[1].x + self.source.width() as u16 + 1,
+            // Move one line down, from the border to the input line
+            layout[1].y + 1,
+        );
+
+        let sexp: Vec<ListItem> = self
+            .sexp
+            .lines()
+            .map(|line| {
+                let content = vec![Spans::from(Span::raw(line))];
+                ListItem::new(content)
+            })
+            .collect();
+        let sexp =
+            List::new(sexp).block(Block::default().borders(Borders::ALL).title("S-expression"));
+        f.render_widget(sexp, layout[2]);
+    }
+
+    fn run<B: Backend>(mut self, term: &mut Terminal<B>) -> Result<()> {
+        loop {
+            term.draw(|f| self.render(f))?;
+
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char(c) => self.calc(Edit::Char(c)),
+                    KeyCode::Backspace => self.calc(Edit::Del),
+                    KeyCode::Esc => return Ok(()),
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut term = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::new()?;
+    let res = app.run(&mut term);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        term.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    term.show_cursor()?;
+
+    res
 }
