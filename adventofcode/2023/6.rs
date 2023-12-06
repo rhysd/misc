b16f8d92579a87e3bb763402b61a7911e23ead15
rhysd <lin90162@yahoo.co.jp>
2023-12-06 23:10:08 +0900

    aoc: make day6 part2 8.7x faster

--- adventofcode/2023/6.rs
+++ adventofcode/2023/6.rs
@@ -1,7 +1,8 @@
+use std::cmp::Ordering;
 use std::env;
 use std::io::{self, BufRead};
 
-fn solve(time: &str, dist: &str) -> usize {
+fn parse<'a>(time: &'a str, dist: &'a str) -> impl Iterator<Item = (usize, usize)> + 'a {
     let time = time
         .strip_prefix("Time:")
         .unwrap()
@@ -13,25 +14,40 @@ fn solve(time: &str, dist: &str) -> usize {
         .split_whitespace()
         .map(|n| n.parse::<usize>().unwrap());
     time.zip(record)
-        .map(|(time, record)| {
-            (1..time)
-                .map(|velocity| velocity * (time - velocity))
-                .filter(|dist| *dist > record)
-                .count()
-        })
-        .product()
+}
+
+fn solve(time: usize, record: usize) -> usize {
+    fn search(min: usize, max: usize, time: usize, record: usize) -> usize {
+        if max - min <= 1 {
+            return max;
+        }
+        let v = (min + max) / 2;
+        match (v * (time - v)).cmp(&record) {
+            Ordering::Equal => v + 1,
+            Ordering::Less => search(v, max, time, record),
+            Ordering::Greater => search(min, v, time, record),
+        }
+    }
+
+    let max = time / 2;
+    let min = search(1, max, time, record);
+    (max - min + 1) * 2 - (time - 1) % 2
 }
 
 fn part1(mut lines: impl Iterator<Item = String>) {
     let time = lines.next().unwrap();
     let dist = lines.next().unwrap();
-    println!("{}", solve(&time, &dist));
+    let result: usize = parse(&time, &dist)
+        .map(|(time, record)| solve(time, record))
+        .product();
+    println!("{}", result);
 }
 
 fn part2(mut lines: impl Iterator<Item = String>) {
     let time = lines.next().unwrap().replace(' ', "");
     let dist = lines.next().unwrap().replace(' ', "");
-    println!("{}", solve(&time, &dist));
+    let (time, record) = parse(&time, &dist).next().unwrap();
+    println!("{}", solve(time, record));
 }
 
 fn main() {
