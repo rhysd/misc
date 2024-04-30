7fb197c44216d3e1170eff7fa4f09c18b59a351d
rhysd <lin90162@yahoo.co.jp>
2024-05-01 03:22:50 +0900

    db: part 5: read from / write to file for persistent DB

--- db_tutorial/src/main.rs
+++ db_tutorial/src/main.rs
@@ -1,30 +1,53 @@
+use std::array;
+use std::env;
 use std::fmt;
-use std::io::{self, BufRead, Write};
+use std::fs::File;
+use std::io::{self, BufRead, Read, Seek, SeekFrom, Write};
 use std::iter;
 use std::mem;
+use std::path::Path;
+use std::slice;
 use std::str;
 
-enum Error<'input> {
-    Unknown(&'input str),
+enum Error {
+    Unknown(String),
     Syntax(&'static str),
     TableFull,
     StringTooLong(u32),
+    PageOutOfBounds(u32),
+    Io(io::Error),
 }
 
-impl<'input> fmt::Display for Error<'input> {
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Unknown(cmd) => write!(f, "Unrecognized command: {cmd:?}"),
             Self::Syntax(usage) => write!(f, "Syntax error: {usage:?}"),
             Self::TableFull => write!(f, "Table is full"),
             Self::StringTooLong(max) => write!(f, "String length exceeds max length {max}"),
+            Self::PageOutOfBounds(idx) => write!(f, "Page index {idx} is out of bounds"),
+            Self::Io(inner) => write!(f, "I/O error: {inner}"),
         }
     }
 }
 
-type Result<'a, T> = std::result::Result<T, Error<'a>>;
+type Result<T> = std::result::Result<T, Box<Error>>;
+
+impl Error {
+    #[inline]
+    fn err<T>(self) -> Result<T> {
+        Err(Box::new(self))
+    }
+}
+
+impl From<io::Error> for Box<Error> {
+    fn from(err: io::Error) -> Self {
+        Box::new(Error::Io(err))
+    }
+}
 
 // TODO: This must be allocated on heap since table row structure will be dynamic
+#[repr(transparent)]
 struct InlineString<const N: usize>([u8; N]);
 
 impl<const N: usize> Default for InlineString<N> {
@@ -34,10 +57,10 @@ impl<const N: usize> Default for InlineString<N> {
 }
 
 impl<const N: usize> InlineString<N> {
-    fn new(s: &str) -> Result<'_, Self> {
+    fn new(s: &str) -> Result<Self> {
         let s = s.as_bytes();
         if s.len() > N {
-            return Err(Error::StringTooLong(N as u32));
+            return Error::StringTooLong(N as u32).err();
         }
 
         Ok(Self(std::array::from_fn(|i| s.get(i).copied().unwrap_or(0))))
@@ -106,6 +129,7 @@ impl<'a> fmt::Display for Row<'a> {
     }
 }
 
+#[repr(C)]
 #[repr(packed(1))]
 #[derive(Default)]
 struct SerializedRow {
@@ -113,12 +137,11 @@ struct SerializedRow {
     user_name: InlineString<32>, // varchar(32)
     email: InlineString<255>,    // varchar(255)
 }
-const _: () = {
-    assert!(mem::size_of::<SerializedRow>() == 291,);
-};
 
 impl SerializedRow {
-    fn serialize<'a>(row: &Row<'a>) -> Result<'a, Self> {
+    const SIZE: usize = mem::size_of::<SerializedRow>();
+
+    fn serialize(row: &Row<'_>) -> Result<Self> {
         Ok(Self {
             id: row.id,
             user_name: InlineString::new(row.user_name)?,
@@ -130,43 +153,158 @@ impl SerializedRow {
         Row { id: self.id, user_name: self.user_name.as_str(), email: self.email.as_str() }
     }
 }
+const _: () = assert!(SerializedRow::SIZE == 291);
 
 #[derive(Default)]
 struct Page(Vec<SerializedRow>);
 
 impl Page {
     const PAGE_SIZE: usize = 4096;
-    const ROWS_PER_PAGE: usize = Self::PAGE_SIZE / mem::size_of::<SerializedRow>();
+    const ROWS_PER_PAGE: usize = Self::PAGE_SIZE / SerializedRow::SIZE;
+
+    fn read_from<R: Read>(mut reader: R) -> io::Result<Self> {
+        let mut rows: Vec<_> =
+            iter::repeat_with(SerializedRow::default).take(Self::ROWS_PER_PAGE).collect();
+        let rows_array: &mut [SerializedRow; Self::ROWS_PER_PAGE] =
+            rows.as_mut_slice().try_into().unwrap();
+        // Safety: This operation is safe because any bit pattern is valid for `SerializedRow` instance.
+        let bytes = unsafe {
+            mem::transmute::<
+                &mut [SerializedRow; Self::ROWS_PER_PAGE],
+                &mut [u8; mem::size_of::<[SerializedRow; Self::ROWS_PER_PAGE]>()],
+            >(rows_array)
+        };
+        // `read_exact` is not available since the last page may not be full.
+        let _ = reader.read(bytes)?;
+        Ok(Self(rows))
+    }
+
+    fn write_to<W: Write>(&self, mut writer: W, num_rows: usize) -> io::Result<()> {
+        assert!(num_rows <= self.0.len(), "{} is out of bounds {}", num_rows, self.0.len());
+        let src_ptr = self.0.as_ptr();
+        // Safety: This operation is safe because the length of array pointed by `src_ptr` and the
+        // length of `bytes` are the same.
+        let bytes = unsafe {
+            let bytes_ptr = mem::transmute::<_, *const u8>(src_ptr);
+            slice::from_raw_parts(bytes_ptr, num_rows * SerializedRow::SIZE)
+        };
+        writer.write_all(bytes)
+    }
 
     fn row_at(&mut self, idx: usize) -> &mut SerializedRow {
-        self.0.reserve_exact(Self::ROWS_PER_PAGE);
-        if idx + 1 > self.0.len() {
-            let diff = idx + 1 - self.0.len();
-            self.0.extend(iter::repeat_with(SerializedRow::default).take(diff));
+        // `self.0` must be filled with zero-initialized SerializedRow instances. Otherwise, this
+        // page read from a file will contain uninitialized instances. Accessing such instances
+        // causes UB.
+        if self.0.is_empty() {
+            self.0.reserve_exact(Self::ROWS_PER_PAGE);
+            self.0.extend(iter::repeat_with(SerializedRow::default).take(Self::ROWS_PER_PAGE));
         }
         &mut self.0[idx]
     }
-}
 
-struct Table {
-    num_rows: u32,
-    pages: [Page; Self::MAX_PAGES],
-}
-
-impl Default for Table {
-    fn default() -> Self {
-        Self { num_rows: 0, pages: std::array::from_fn(|_| Page::default()) }
+    fn is_cached(&self) -> bool {
+        !self.0.is_empty()
     }
 }
 
-impl Table {
-    const MAX_PAGES: usize = 100;
-    const MAX_ROWS: usize = Page::ROWS_PER_PAGE * Self::MAX_PAGES;
+struct Pager {
+    file: Option<File>,
+    file_len: u64,
+    pages: [Page; Self::MAX_PAGES],
+}
 
-    fn row_slot(&mut self, row_num: u32) -> &mut SerializedRow {
+impl Default for Pager {
+    fn default() -> Self {
+        let pages = array::from_fn(|_| Page::default());
+        Self { file: None, file_len: 0, pages }
+    }
+}
+
+impl Pager {
+    const MAX_PAGES: usize = 100;
+
+    fn open(path: &Path) -> io::Result<Self> {
+        let file =
+            File::options().read(true).write(true).create(true).truncate(false).open(path)?;
+        let file_len = file.metadata()?.len();
+        let pages = std::array::from_fn(|_| Page::default());
+        Ok(Self { file: Some(file), file_len, pages })
+    }
+
+    fn page_at(&mut self, page_num: usize) -> Result<&mut Page> {
+        let Some(page) = self.pages.get_mut(page_num) else {
+            return Error::PageOutOfBounds(page_num as u32).err();
+        };
+        if !page.is_cached() {
+            if let Some(file) = &mut self.file {
+                let num_pages = self.file_len as usize / Page::PAGE_SIZE
+                    + usize::from(self.file_len as usize % Page::PAGE_SIZE > 0);
+                if page_num <= num_pages {
+                    let offset = page_num * Page::PAGE_SIZE;
+                    file.seek(SeekFrom::Start(offset as u64))?;
+                    *page = Page::read_from(file)?;
+                }
+            }
+        }
+        Ok(page)
+    }
+
+    fn flush(&mut self, page_num: usize, rows: usize) -> io::Result<()> {
+        let Some(file) = &mut self.file else {
+            return Ok(());
+        };
+
+        let Some(page) = self.pages.get_mut(page_num) else {
+            return Ok(());
+        };
+
+        if !page.is_cached() {
+            return Ok(());
+        }
+
+        let offset = page_num * Page::PAGE_SIZE;
+        file.seek(SeekFrom::Start(offset as u64))?;
+        page.write_to(file, rows)?;
+
+        *page = Page::default();
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+struct Table {
+    num_rows: u32,
+    pager: Pager,
+}
+
+impl Table {
+    const MAX_ROWS: usize = Page::ROWS_PER_PAGE * Pager::MAX_PAGES;
+
+    fn open(path: &Path) -> io::Result<Self> {
+        let pager = Pager::open(path)?;
+        let num_rows = (pager.file_len / SerializedRow::SIZE as u64) as u32;
+        Ok(Self { num_rows, pager })
+    }
+
+    fn row_slot(&mut self, row_num: u32) -> Result<&mut SerializedRow> {
         let page_num = row_num as usize / Page::ROWS_PER_PAGE;
-        let page = &mut self.pages[page_num];
-        page.row_at(row_num as usize % Page::ROWS_PER_PAGE)
+        let page = self.pager.page_at(page_num)?;
+        let row = page.row_at(row_num as usize % Page::ROWS_PER_PAGE);
+        Ok(row)
+    }
+
+    fn close(&mut self) -> io::Result<()> {
+        let num_full_pages = self.num_rows as usize / Page::ROWS_PER_PAGE;
+        for idx in 0..num_full_pages {
+            self.pager.flush(idx, Page::ROWS_PER_PAGE)?;
+        }
+
+        let num_additional_rows = self.num_rows as usize % Page::ROWS_PER_PAGE;
+        if num_additional_rows > 0 {
+            self.pager.flush(num_full_pages, num_additional_rows)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -177,10 +315,10 @@ enum Statement<'input> {
 }
 
 impl<'input> Statement<'input> {
-    fn prepare(input: &'input str) -> Result<'input, Self> {
+    fn prepare(input: &'input str) -> Result<Self> {
         let mut tokens = input.split_whitespace();
         let Some(cmd) = tokens.next() else {
-            return Err(Error::Unknown(""));
+            return Error::Unknown(String::new()).err();
         };
         match cmd {
             "insert" => {
@@ -191,21 +329,21 @@ impl<'input> Statement<'input> {
                     let row = Row { id, user_name, email };
                     Some(Statement::Insert(row))
                 };
-                parse().ok_or(Error::Syntax("insert {id} {user} {email}"))
+                parse().ok_or_else(|| Box::new(Error::Syntax("insert {id} {user} {email}")))
             }
             "select" => Ok(Self::Select),
-            c => Err(Error::Unknown(c)),
+            c => Error::Unknown(c.to_string()).err(),
         }
     }
 
-    fn execute<W: Write>(&self, table: &mut Table, mut w: W) -> Result<'input, ()> {
+    fn execute<W: Write>(&self, table: &mut Table, mut w: W) -> Result<()> {
         match self {
             Self::Insert(row) => {
                 if table.num_rows as usize >= Table::MAX_ROWS {
-                    return Err(Error::TableFull);
+                    return Error::TableFull.err();
                 }
 
-                let slot = table.row_slot(table.num_rows);
+                let slot = table.row_slot(table.num_rows)?;
                 *slot = SerializedRow::serialize(row)?;
                 table.num_rows += 1;
 
@@ -213,7 +351,7 @@ impl<'input> Statement<'input> {
             }
             Self::Select => {
                 for i in 0..table.num_rows {
-                    let row = table.row_slot(i).deserialize();
+                    let row = table.row_slot(i)?.deserialize();
                     writeln!(w, "{row}").unwrap();
                 }
                 Ok(())
@@ -222,8 +360,7 @@ impl<'input> Statement<'input> {
     }
 }
 
-fn run<R: BufRead, W: Write>(mut stdin: R, mut stdout: W) -> io::Result<()> {
-    let mut table = Table::default();
+fn repl<R: BufRead, W: Write>(mut stdin: R, mut stdout: W, mut table: Table) -> io::Result<()> {
     let mut prompt = Prompt::default();
 
     loop {
@@ -235,6 +372,7 @@ fn run<R: BufRead, W: Write>(mut stdin: R, mut stdout: W) -> io::Result<()> {
                 };
                 match cmd {
                     MetaCommand::Exit => {
+                        table.close()?;
                         writeln!(stdout, "Bye.").unwrap();
                         break;
                     }
@@ -256,7 +394,12 @@ fn run<R: BufRead, W: Write>(mut stdin: R, mut stdout: W) -> io::Result<()> {
 }
 
 fn main() -> io::Result<()> {
-    run(io::stdin().lock(), io::stdout())
+    let table = if let Some(path) = env::args_os().nth(1) {
+        Table::open(Path::new(&path))?
+    } else {
+        Table::default()
+    };
+    repl(io::stdin().lock(), io::stdout(), table)
 }
 
 #[cfg(test)]
@@ -272,7 +415,7 @@ mod tests {
         let stdin = stdin.as_ref();
         let mut stdout = Vec::<u8>::new();
         let mut stdin = BufReader::new(stdin.as_bytes());
-        run(&mut stdin, &mut stdout)?;
+        repl(&mut stdin, &mut stdout, Table::default())?;
         Ok(String::from_utf8(stdout).unwrap())
     }
 
