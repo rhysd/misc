50d3950b8e9f63b2d4ba1abdf54adf29a6a27f65
rhysd <lin90162@yahoo.co.jp>
2024-12-14 16:24:05 +0900

    aoc: make day14 251x faster
    ```
    $ hyperfine './14_before 2 < in/14-1' './14_after 2 < in/14-1'
    Benchmark 1: ./14_before 2 < in/14-1
      Time (mean ± σ):      8.561 s ±  0.024 s    [User: 8.549 s, System: 0.005 s]
      Range (min … max):    8.545 s …  8.624 s    10 runs
    
    Benchmark 2: ./14_after 2 < in/14-1
      Time (mean ± σ):      34.0 ms ±   0.4 ms    [User: 31.8 ms, System: 1.2 ms]
      Range (min … max):    33.1 ms …  35.8 ms    80 runs
    
    Summary
      ./14_after 2 < in/14-1 ran
      251.88 ± 3.34 times faster than ./14_before 2 < in/14-1
    ```

--- adventofcode/2024/14.rs
+++ adventofcode/2024/14.rs
@@ -1,140 +1,136 @@
 use std::env;
 use std::io::{self, BufRead};
+use std::ops::Range;
 
 type Pos = (i32, i32);
 
-fn part1(lines: impl Iterator<Item = String>) {
-    let mut robots: Vec<_> = lines
-        .map(|l| {
-            fn parse_pos(s: &str) -> Pos {
-                let mut s = s.split(',');
-                (s.next().unwrap().parse().unwrap(), s.next().unwrap().parse().unwrap())
-            }
-            let mut s = l.split_whitespace();
-            (
-                parse_pos(s.next().unwrap().strip_prefix("p=").unwrap()),
-                parse_pos(s.next().unwrap().strip_prefix("v=").unwrap()),
-            )
-        })
-        .collect();
+const WIDTH: i32 = 101;
+const HEIGHT: i32 = 103;
 
-    let width = 101;
-    let height = 103;
-    for _ in 0..100 {
-        for r in robots.iter_mut() {
+struct Robots(Vec<(Pos, Pos)>);
+
+impl Robots {
+    fn parse(lines: impl Iterator<Item = String>) -> Self {
+        let robo = lines
+            .map(|l| {
+                let mut s = l.split_whitespace();
+                let mut pos = |label| {
+                    let s = s.next().unwrap();
+                    let mut s = s.strip_prefix(label).unwrap().split(',');
+                    (s.next().unwrap().parse().unwrap(), s.next().unwrap().parse().unwrap())
+                };
+                (pos("p="), pos("v="))
+            })
+            .collect();
+
+        Self(robo)
+    }
+
+    fn update(&mut self) {
+        for r in self.0.iter_mut() {
             let ((x, y), (dx, dy)) = *r;
+            // Note: Using `rem_euclid` is smarter but it is 14% slower probably due to extra non-zero check on rhs.
+            // ```
+            // r.0 = ((x + dx).rem_euclid(WIDTH), (y + dy).rem_euclid(HEIGHT));
+            // ```
             let (mut x, mut y) = (x + dx, y + dy);
             if x < 0 {
-                x += width;
-            } else if width <= x {
-                x -= width;
+                x += WIDTH;
+            } else if WIDTH <= x {
+                x -= WIDTH;
             }
             if y < 0 {
-                y += height;
-            } else if height <= y {
-                y -= height;
+                y += HEIGHT;
+            } else if HEIGHT <= y {
+                y -= HEIGHT;
             }
             r.0 = (x, y);
         }
     }
 
+    fn count_within(&self, xr: Range<i32>, yr: Range<i32>) -> usize {
+        self.0.iter().filter(|&((x, y), _)| xr.contains(x) && yr.contains(y)).count()
+    }
+
+    fn is_merry_christmas(&self) -> bool {
+        const TREE: &[&[u8]] = &[
+            b"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            b"X                             X",
+            b"X                             X",
+            b"X                             X",
+            b"X                             X",
+            b"X              X              X",
+            b"X             XXX             X",
+            b"X            XXXXX            X",
+            b"X           XXXXXXX           X",
+            b"X          XXXXXXXXX          X",
+            b"X            XXXXX            X",
+            b"X           XXXXXXX           X",
+            b"X          XXXXXXXXX          X",
+            b"X         XXXXXXXXXXX         X",
+            b"X        XXXXXXXXXXXXX        X",
+            b"X          XXXXXXXXX          X",
+            b"X         XXXXXXXXXXX         X",
+            b"X        XXXXXXXXXXXXX        X",
+            b"X       XXXXXXXXXXXXXXX       X",
+            b"X      XXXXXXXXXXXXXXXXX      X",
+            b"X        XXXXXXXXXXXXX        X",
+            b"X       XXXXXXXXXXXXXXX       X",
+            b"X      XXXXXXXXXXXXXXXXX      X",
+            b"X     XXXXXXXXXXXXXXXXXXX     X",
+            b"X    XXXXXXXXXXXXXXXXXXXXX    X",
+            b"X             XXX             X",
+            b"X             XXX             X",
+            b"X             XXX             X",
+            b"X                             X",
+            b"X                             X",
+            b"X                             X",
+            b"X                             X",
+            b"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        ];
+
+        let mut rendered = [[b' '; WIDTH as usize]; HEIGHT as usize];
+        for &((x, y), _) in &self.0 {
+            rendered[y as usize][x as usize] = b'X';
+        }
+
+        rendered.windows(TREE.len()).any(|ys| {
+            let Some(x) = ys[0].windows(TREE[0].len()).position(|w| w == TREE[0]) else {
+                return false;
+            };
+            ys.iter().enumerate().skip(1).all(|(y, xs)| xs[x..].starts_with(TREE[y]))
+        })
+    }
+}
+
+fn part1(lines: impl Iterator<Item = String>) {
+    let mut robots = Robots::parse(lines);
+
+    // Note: This is as fast as `(x + dx * 100).rem_euclid(WIDTH)` thanks to optimizations.
+    for _ in 0..100 {
+        robots.update();
+    }
+
     let factor: usize = [
-        (0..width / 2, 0..height / 2),
-        (width / 2 + 1..width, 0..height / 2),
-        (0..width / 2, height / 2 + 1..height),
-        (width / 2 + 1..width, height / 2 + 1..height),
+        (0..WIDTH / 2, 0..HEIGHT / 2),
+        (WIDTH / 2 + 1..WIDTH, 0..HEIGHT / 2),
+        (0..WIDTH / 2, HEIGHT / 2 + 1..HEIGHT),
+        (WIDTH / 2 + 1..WIDTH, HEIGHT / 2 + 1..HEIGHT),
     ]
     .into_iter()
-    .map(|(xr, yr)| robots.iter().filter(|&((x, y), _)| xr.contains(x) && yr.contains(y)).count())
+    .map(|(xr, yr)| robots.count_within(xr, yr))
     .product();
 
     println!("{factor}");
 }
 
-const THE_TREE: &[&[u8]] = &[
-    b"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    b"X                             X",
-    b"X                             X",
-    b"X                             X",
-    b"X                             X",
-    b"X              X              X",
-    b"X             XXX             X",
-    b"X            XXXXX            X",
-    b"X           XXXXXXX           X",
-    b"X          XXXXXXXXX          X",
-    b"X            XXXXX            X",
-    b"X           XXXXXXX           X",
-    b"X          XXXXXXXXX          X",
-    b"X         XXXXXXXXXXX         X",
-    b"X        XXXXXXXXXXXXX        X",
-    b"X          XXXXXXXXX          X",
-    b"X         XXXXXXXXXXX         X",
-    b"X        XXXXXXXXXXXXX        X",
-    b"X       XXXXXXXXXXXXXXX       X",
-    b"X      XXXXXXXXXXXXXXXXX      X",
-    b"X        XXXXXXXXXXXXX        X",
-    b"X       XXXXXXXXXXXXXXX       X",
-    b"X      XXXXXXXXXXXXXXXXX      X",
-    b"X     XXXXXXXXXXXXXXXXXXX     X",
-    b"X    XXXXXXXXXXXXXXXXXXXXX    X",
-    b"X             XXX             X",
-    b"X             XXX             X",
-    b"X             XXX             X",
-    b"X                             X",
-    b"X                             X",
-    b"X                             X",
-    b"X                             X",
-    b"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-];
-
 fn part2(lines: impl Iterator<Item = String>) {
-    let mut robots: Vec<_> = lines
-        .map(|l| {
-            fn parse_pos(s: &str) -> Pos {
-                let mut s = s.split(',');
-                (s.next().unwrap().parse().unwrap(), s.next().unwrap().parse().unwrap())
-            }
-            let mut s = l.split_whitespace();
-            (
-                parse_pos(s.next().unwrap().strip_prefix("p=").unwrap()),
-                parse_pos(s.next().unwrap().strip_prefix("v=").unwrap()),
-            )
-        })
-        .collect();
-
-    let width = 101;
-    let height = 103;
-    for i in 0..10000 {
-        for r in robots.iter_mut() {
-            let ((x, y), (dx, dy)) = *r;
-            let (mut x, mut y) = (x + dx, y + dy);
-            if x < 0 {
-                x += width;
-            } else if width <= x {
-                x -= width;
-            }
-            if y < 0 {
-                y += height;
-            } else if height <= y {
-                y -= height;
-            }
-            r.0 = (x, y);
-        }
-
-        for y in 0..height - THE_TREE.len() as i32 {
-            'tile: for x in 0..width - THE_TREE[0].len() as i32 {
-                for (ty, txs) in THE_TREE.iter().enumerate() {
-                    for (tx, &b) in txs.iter().enumerate() {
-                        let (x, y) = (x + tx as i32, y + ty as i32);
-                        let is_robot = robots.iter().any(|&(p, _)| p == (x, y));
-                        if b == b' ' && is_robot || b == b'X' && !is_robot {
-                            continue 'tile;
-                        }
-                    }
-                }
-                println!("{}", i + 1);
-                return;
-            }
+    let mut robots = Robots::parse(lines);
+    for secs in 1.. {
+        robots.update();
+        if robots.is_merry_christmas() {
+            println!("{secs}");
+            return;
         }
     }
 }
