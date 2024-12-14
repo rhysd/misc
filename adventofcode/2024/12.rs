d7ce7bda04f2aac8897757edf96dbbe67760b729
rhysd <lin90162@yahoo.co.jp>
2024-12-14 22:53:10 +0900

    aoc: make day12 1.50x faster
    by calculating corners directly
    
    ```
    $ hyperfine './12_before 2 < in/12-1' './12_after 2 < in/12-1'
    Benchmark 1: ./12_before 2 < in/12-1
      Time (mean ± σ):       9.4 ms ±   0.3 ms    [User: 7.2 ms, System: 1.3 ms]
      Range (min … max):     8.9 ms …  11.3 ms    240 runs
    
    Benchmark 2: ./12_after 2 < in/12-1
      Time (mean ± σ):       6.3 ms ±   0.2 ms    [User: 4.2 ms, System: 1.3 ms]
      Range (min … max):     5.9 ms …   7.0 ms    308 runs
    
    Summary
      ./12_after 2 < in/12-1 ran
        1.50 ± 0.06 times faster than ./12_before 2 < in/12-1
    ```

--- adventofcode/2024/12.rs
+++ adventofcode/2024/12.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 use std::env;
 use std::io::{self, BufRead};
 
@@ -48,51 +48,53 @@ fn part2(lines: impl Iterator<Item = String>) {
 
     let mut visited = HashSet::new();
     let mut price_at = move |x: usize, y: usize| {
+        if !visited.insert((x, y)) {
+            return 0; // Avoid memory allocation of `VecDeque`
+        }
+
         let c = arrange[y][x];
         let mut queue = VecDeque::from([(x, y)]);
-        let mut points = HashMap::new(); // Map from point to its edges
         let mut area: usize = 0;
+        let mut corners: usize = 0;
         while let Some((x, y)) = queue.pop_front() {
-            if visited.contains(&(x, y)) {
-                continue;
-            }
-            visited.insert((x, y));
             area += 1;
-            for (pos, (from, to)) in [
-                ((y > 0).then(|| (x, y - 1)), ((x, y), (x + 1, y))),
-                ((x + 1 < xlen).then(|| (x + 1, y)), ((x + 1, y), (x + 1, y + 1))),
-                ((y + 1 < ylen).then(|| (x, y + 1)), ((x, y + 1), (x + 1, y + 1))),
-                ((x > 0).then(|| (x - 1, y)), ((x, y), (x, y + 1))),
-            ] {
-                match pos {
-                    Some((x, y)) if arrange[y][x] == c => {
-                        queue.push_back((x, y));
+
+            let neighbors = [
+                (y > 0).then(|| (x, y - 1)),
+                (x + 1 < xlen).then(|| (x + 1, y)),
+                (y + 1 < ylen).then(|| (x, y + 1)),
+                (x > 0).then(|| (x - 1, y)),
+            ];
+            for (i, cur) in neighbors.into_iter().enumerate() {
+                // Looking at the top-right corner, check if it is one of the following patterns where A is the current
+                // cell and B is other cell and E is empty (outside the field).
+                //
+                //   AB   B    E    B    E
+                //   AA   AB   AB   AE   AE
+                //
+                // Do this for all four corners.
+                let is_corner = match (cur, neighbors[(i + 1) % 4]) {
+                    (Some((x1, y1)), Some((x2, y2))) => {
+                        let (x3, y3) = if (x, y) != (x1, y2) { (x1, y2) } else { (x2, y1) }; // Diagonal position
+                        let (c1, c2, c3) = (arrange[y1][x1], arrange[y2][x2], arrange[y3][x3]);
+                        c != c1 && c != c2 || c == c1 && c == c2 && c != c3
                     }
-                    _ => {
-                        points.entry(from).or_insert(vec![]).push(to);
-                        points.entry(to).or_insert(vec![]).push(from);
+                    (Some((x, y)), None) | (None, Some((x, y))) => arrange[y][x] != c,
+                    (None, None) => true,
+                };
+                if is_corner {
+                    corners += 1;
+                }
+
+                if let Some((x, y)) = cur {
+                    if arrange[y][x] == c && visited.insert((x, y)) {
+                        queue.push_back((x, y));
                     }
                 }
             }
         }
 
         // The number of corners is equal to the number of sides.
-        let corners: usize = points
-            .into_values()
-            .map(|edges| match edges.len() {
-                4 => 2, // 4 edges at a single point means two corners are tangent
-                2 => {
-                    let ((x1, y1), (x2, y2)) = (edges[0], edges[1]);
-                    if x1 == x2 || y1 == y2 {
-                        0 // Straight
-                    } else {
-                        1 // Orthogonal
-                    }
-                }
-                _ => unreachable!(),
-            })
-            .sum();
-
         corners * area
     };
 
