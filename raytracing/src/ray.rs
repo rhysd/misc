aa64970ee823799cf06c8feef566cdb6e7d05633
rhysd <lin90162@yahoo.co.jp>
2025-04-20 22:36:44 +0900

    raytracing: implement generic `Hittable` trait

--- raytracing/src/ray.rs
+++ raytracing/src/ray.rs
@@ -1,16 +1,49 @@
 use crate::color::Color;
 use crate::vec3::{Point3, Vec3};
+use std::ops::RangeBounds;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> Option<f64> {
-    let oc = *center - *r.origin(); // C - Q
-    let a = r.direction().length_squared();
-    let h = r.direction().dot(&oc);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = h * h - a * c;
-    (discriminant >= 0.0).then(|| {
-        // Return `t` when the ray hits the sphere for the first time
-        (h - discriminant.sqrt()) / a
-    })
+pub struct Hit {
+    pub pos: Point3,
+    pub normal: Vec3,
+    pub time: f64,
+}
+
+pub trait Hittable {
+    fn hit<R: RangeBounds<f64>>(&self, ray: &Ray, time: R) -> Option<Hit>;
+}
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self {
+            center,
+            radius: radius.max(0.0),
+        }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit<R: RangeBounds<f64>>(&self, ray: &Ray, time: R) -> Option<Hit> {
+        let oc = self.center - *ray.origin(); // C - Q
+        let a = ray.direction().length_squared();
+        let h = ray.direction().dot(&oc);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = h * h - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let d = discriminant.sqrt();
+        let time = [h - d, h + d].into_iter().find(|t| time.contains(t))?;
+        let pos = ray.at(time);
+        let normal = (pos - self.center) / self.radius;
+
+        Some(Hit { time, pos, normal })
+    }
 }
 
 #[derive(Default)]
@@ -37,8 +70,9 @@ impl Ray {
     }
 
     pub fn color(&self) -> Color {
-        if let Some(t) = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, self) {
-            let n = (self.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+        let sphere = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5);
+        if let Some(Hit { time, .. }) = sphere.hit(self, 0.0..100.0) {
+            let n = (self.at(time) - Vec3::new(0.0, 0.0, -1.0)).unit();
             let v = 0.5 * (n + 1.0);
             return Color::new(v.x(), v.y(), v.z());
         }
