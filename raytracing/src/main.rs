368fc79107e3e9c3823f8b00633ace767952ea8d
rhysd <lin90162@yahoo.co.jp>
2025-04-22 18:47:34 +0900

    raytracing: implement generic multiple hittable objects

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -2,7 +2,7 @@ mod color;
 mod ray;
 mod vec3;
 
-use ray::Ray;
+use ray::{Hittables, Ray, Sphere};
 use std::fs::File;
 use std::io::{self, BufWriter, Write};
 use vec3::{Point3, Vec3};
@@ -36,12 +36,16 @@ fn main() -> io::Result<()> {
     writeln!(f, "{IMAGE_WIDTH} {IMAGE_HEIGHT}")?;
     writeln!(f, "255")?;
 
+    let mut world = Hittables::default();
+    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
+    world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
+
     for h in 0..IMAGE_HEIGHT {
         for w in 0..IMAGE_WIDTH {
             let pixel_center = pixel_00_loc + w as f64 * pixel_delta_u + h as f64 * pixel_delta_v;
             let ray_direction = pixel_center - CAMERA_CENTER;
             Ray::new(CAMERA_CENTER, ray_direction)
-                .color()
+                .color(&world)
                 .write_to(&mut f)?;
         }
     }
--- raytracing/src/ray.rs
+++ raytracing/src/ray.rs
@@ -1,15 +1,22 @@
 use crate::color::Color;
 use crate::vec3::{Point3, Vec3};
-use std::ops::RangeBounds;
+use std::ops::{Deref, DerefMut, Range};
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Face {
+    Front,
+    Back,
+}
 
 pub struct Hit {
     pub pos: Point3,
     pub normal: Vec3,
     pub time: f64,
+    pub face: Face,
 }
 
 pub trait Hittable {
-    fn hit<R: RangeBounds<f64>>(&self, ray: &Ray, time: R) -> Option<Hit>;
+    fn hit(&self, ray: &Ray, time: Range<f64>) -> Option<Hit>;
 }
 
 pub struct Sphere {
@@ -27,7 +34,7 @@ impl Sphere {
 }
 
 impl Hittable for Sphere {
-    fn hit<R: RangeBounds<f64>>(&self, ray: &Ray, time: R) -> Option<Hit> {
+    fn hit(&self, ray: &Ray, time: Range<f64>) -> Option<Hit> {
         let oc = self.center - *ray.origin(); // C - Q
         let a = ray.direction().length_squared();
         let h = ray.direction().dot(&oc);
@@ -41,8 +48,46 @@ impl Hittable for Sphere {
         let time = [h - d, h + d].into_iter().find(|t| time.contains(t))?;
         let pos = ray.at(time);
         let normal = (pos - self.center) / self.radius;
+        let face = ray.face(&normal);
 
-        Some(Hit { time, pos, normal })
+        Some(Hit {
+            time,
+            pos,
+            normal,
+            face,
+        })
+    }
+}
+
+#[derive(Default)]
+pub struct Hittables(Vec<Box<dyn Hittable>>);
+
+impl Hittables {
+    pub fn add<H: Hittable + 'static>(&mut self, h: H) {
+        self.0.push(Box::new(h));
+    }
+}
+
+impl Deref for Hittables {
+    type Target = Vec<Box<dyn Hittable>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Hittables {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Hittable for Hittables {
+    fn hit(&self, ray: &Ray, time: Range<f64>) -> Option<Hit> {
+        self.0
+            .iter()
+            .flat_map(|h| h.hit(ray, time.clone()))
+            .min_by(|l, r| l.time.partial_cmp(&r.time).unwrap())
     }
 }
 
@@ -69,11 +114,9 @@ impl Ray {
         self.orig + t * self.dir
     }
 
-    pub fn color(&self) -> Color {
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5);
-        if let Some(Hit { time, .. }) = sphere.hit(self, 0.0..100.0) {
-            let n = (self.at(time) - Vec3::new(0.0, 0.0, -1.0)).unit();
-            let v = 0.5 * (n + 1.0);
+    pub fn color<H: Hittable>(&self, world: &H) -> Color {
+        if let Some(Hit { normal, .. }) = world.hit(self, 0.0..std::f64::INFINITY) {
+            let v = 0.5 * (normal + 1.0);
             return Color::new(v.x(), v.y(), v.z());
         }
 
@@ -81,5 +124,14 @@ impl Ray {
         let a = u.y() / 2.0 + 1.0;
         let v = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0);
         Color::new(v.x(), v.y(), v.z())
+    }
+
+    fn face(&self, outward_normal: &Vec3) -> Face {
+        // NOTE: `outward_normal` is assumed to have unit length
+        if self.dir.dot(outward_normal) < 0.0 {
+            Face::Front
+        } else {
+            Face::Back
+        }
     }
 }
