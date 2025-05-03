4c017cae4e1ac44523419cf57539ba96f0eb6b8c
rhysd <lin90162@yahoo.co.jp>
2025-05-04 01:08:35 +0900

    raytracing: render final image

---
+++ raytracing/README.md
@@ -0,0 +1,1 @@
+![output](./out.jpg)
--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -8,37 +8,77 @@ mod vec3;
 use camera::Camera;
 use hittable::{Hittables, Sphere};
 use material::{Dielectric, Lambertian, Metal};
+use rand::random_range;
 use std::io;
 use vec3::{Color, Point3, Vec3};
 
 fn main() -> io::Result<()> {
     let mut world = Hittables::default();
 
-    let ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
-    let center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let left = Dielectric::new(1.5);
-    let bubble = Dielectric::new(1.0 / 1.5);
-    let right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
+    // Ground
+    world.add(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        Lambertian::new(Color::new(0.5, 0.5, 0.5)),
+    ));
 
-    world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, ground));
-    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.2), 0.5, center));
-    world.add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, left));
-    world.add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.4, bubble));
-    world.add(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, right));
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = {
+                let x = a as f64 + random_range(0.0..0.9);
+                let z = b as f64 + random_range(0.0..0.9);
+                Point3::new(x, 0.2, z)
+            };
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() <= 0.9 {
+                continue;
+            }
+
+            let random = random_range(0.0..1.0);
+            if random < 0.8 {
+                // Diffuse
+                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
+                let sphere = Sphere::new(center, 0.2, Lambertian::new(albedo));
+                world.add(sphere);
+            } else if random < 0.95 {
+                // Metal
+                let albedo = Color::random(0.5..1.0);
+                let fuzz = random_range(0.0..0.5);
+                let sphere = Sphere::new(center, 0.2, Metal::new(albedo, fuzz));
+                world.add(sphere);
+            } else {
+                // Glass
+                let sphere = Sphere::new(center, 0.2, Dielectric::new(1.5));
+                world.add(sphere);
+            }
+        }
+    }
+
+    world.add(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, Dielectric::new(1.5)));
+    world.add(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Lambertian::new(Color::new(0.4, 0.2, 0.1)),
+    ));
+    world.add(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        Metal::new(Color::new(0.7, 0.6, 0.5), 0.0),
+    ));
 
     let mut cam = Camera::new("out.ppm")?;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20.0;
-    cam.lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    cam.lookat = Point3::new(0.0, 0.0, -1.0);
+    cam.lookfrom = Point3::new(13.0, 2.0, 3.0);
+    cam.lookat = Point3::new(0.0, 0.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_distance = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_distance = 10.0;
 
     cam.render(&world)
 }
