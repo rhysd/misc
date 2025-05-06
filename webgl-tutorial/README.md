e0954cd4ed96261ab5788cbd8aa3df179d5db41f
rhysd <lin90162@yahoo.co.jp>
2025-05-06 22:08:45 +0900

    webgl: initialize project

---
+++ webgl-tutorial/README.md
@@ -0,0 +1,1 @@
+https://wgld.org/d/webgl/
---
+++ webgl-tutorial/script.js
@@ -0,0 +1,9 @@
+(function() {
+    const canvas = document.getElementById('canvas');
+    canvas.width = 500;
+    canvas.height = 300;
+
+    const gl = canvas.getContext('webgl');
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+})();
