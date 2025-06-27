b4936baeeb9e7de0d4724c900871a9ff1f76d5e3
rhysd <lin90162@yahoo.co.jp>
2025-06-27 21:25:38 +0900

    d3d12: initialize D3D12 objects

--- d3d12/HelloWorld/app.cpp
+++ d3d12/HelloWorld/app.cpp
@@ -1,9 +1,22 @@
 #include "App.h"
 #include <Windows.h>
+#include <combaseapi.h>
+#include <d3d12.h>
+#include <dxgi.h>
+#include <dxgiformat.h>
+#include <dxgitype.h>
 
 namespace {
 const auto ClassName = TEXT("Hello, D3D12");
+
+template <class T>
+void Release(T *&p) {
+    if (p != nullptr) {
+        p->Release();
+        p = nullptr;
+    }
 }
+} // namespace
 
 App::App(uint32_t const width, uint32_t const height)
     : hinst_(nullptr), hwnd_(nullptr), width_(width), height_(height) {}
@@ -96,6 +109,166 @@ void App::term_window() {
     }
     hinst_ = nullptr;
     hwnd_ = nullptr;
+}
+
+bool App::init_d3d() {
+    // Create device
+    {
+        auto const hr = D3D12CreateDevice(
+            nullptr,                 // Video adaptor
+            D3D_FEATURE_LEVEL_11_0,  // Minimal feature level
+            IID_PPV_ARGS(&device_)); // Macro equivalent to `__uuidof(&device_), (void **)device_`
+        if (FAILED(hr)) {
+            return false;
+        }
+    }
+
+    // Create command queue to submit draw commands to GPU
+    {
+        D3D12_COMMAND_QUEUE_DESC desc{};
+        desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT; // Commands that GPU executes
+        desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+        desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        desc.NodeMask = 0; // Note: Assumes only single GPU is available
+
+        auto const hr = device_->CreateCommandQueue(&desc, IID_PPV_ARGS(&queue_));
+        if (FAILED(hr)) {
+            return false;
+        }
+    }
+
+    // Create swap chain to swap frame buffers (double buffer)
+    {
+        // Factory object to access DXGI
+        // Note: DXGI = DirectX Graphics Infrastructure
+        IDXGIFactory4 *factory = nullptr;
+        auto hr = CreateDXGIFactory1(IID_PPV_ARGS(&factory));
+        if (FAILED(hr)) {
+            return false;
+        }
+
+        DXGI_SWAP_CHAIN_DESC desc{};
+        desc.BufferDesc.Width = width_;
+        desc.BufferDesc.Height = height_;
+        desc.BufferDesc.RefreshRate.Numerator = 60; // Refresh rate is 60/1
+        desc.BufferDesc.RefreshRate.Denominator = 1;
+        desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+        desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+        desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        desc.SampleDesc.Count = 1; // Number of multi-sampling per pixel
+        desc.SampleDesc.Quality = 0;
+        desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        desc.BufferCount = FRAME_COUNT;
+        desc.OutputWindow = hwnd_;
+        desc.Windowed = TRUE;
+        desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+        desc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // Switch display mode with `IDXGISwapChain::ResizeTarget()`
+
+        IDXGISwapChain *swap_chain = nullptr;
+        hr = factory->CreateSwapChain(queue_, &desc, &swap_chain);
+        if (FAILED(hr)) {
+            Release(factory);
+            return false;
+        }
+
+        hr = swap_chain->QueryInterface(IID_PPV_ARGS(&swap_chain_));
+        if (FAILED(hr)) {
+            Release(factory);
+            Release(swap_chain);
+            return false;
+        }
+
+        frame_index_ = swap_chain_->GetCurrentBackBufferIndex();
+
+        Release(factory);
+        Release(swap_chain);
+    }
+
+    // Create command allocator
+    {
+        for (auto i = 0; i < FRAME_COUNT; i++) {
+            auto const hr = device_->CreateCommandAllocator(
+                D3D12_COMMAND_LIST_TYPE_DIRECT,
+                IID_PPV_ARGS(&cmd_alloc_[i]));
+            if (FAILED(hr)) {
+                return false;
+            }
+        }
+    }
+
+    // Create command list
+    {
+        auto const hr = device_->CreateCommandList(
+            0,
+            D3D12_COMMAND_LIST_TYPE_DIRECT,
+            cmd_alloc_[frame_index_],
+            nullptr,
+            IID_PPV_ARGS(&cmd_list_));
+        if (FAILED(hr)) {
+            return false;
+        }
+    }
+
+    // Create render target view (resource view object)
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC desc{};
+        desc.NumDescriptors = FRAME_COUNT;
+        desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        desc.NodeMask = 0;
+
+        auto const hr = device_->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&heap_rtv_));
+        if (FAILED(hr)) {
+            return false;
+        }
+
+        auto handle = heap_rtv_->GetCPUDescriptorHandleForHeapStart();
+        auto const inc_size = device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        for (auto i = 0; i < FRAME_COUNT; i++) {
+            auto const hr = swap_chain_->GetBuffer(i, IID_PPV_ARGS(&color_buffer_[i]));
+            if (FAILED(hr)) {
+                return false;
+            }
+
+            D3D12_RENDER_TARGET_VIEW_DESC desc{};
+            desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+            desc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+            desc.Texture2D.MipSlice = 0; // Mipmap level (0 = one mipmap)
+            desc.Texture2D.PlaneSlice = 0;
+
+            device_->CreateRenderTargetView(color_buffer_[i], &desc, handle);
+
+            handle_rtv_[i] = handle;
+            handle.ptr += inc_size;
+        }
+    }
+
+    // Create fence
+    {
+        for (auto i = 0; i < FRAME_COUNT; i++) {
+            fence_counter_[i] = 0;
+        }
+
+        auto const hr = device_->CreateFence(
+            fence_counter_[frame_index_],
+            D3D12_FENCE_FLAG_NONE,
+            IID_PPV_ARGS(&fence_));
+        if (FAILED(hr)) {
+            return false;
+        }
+
+        fence_counter_[frame_index_]++;
+
+        fence_event_ = CreateEvent(/* attributes */ nullptr, /* manual reset */ FALSE, /* initial state */ FALSE, /* name */ nullptr);
+        if (fence_event_ == nullptr) {
+            return false;
+        }
+    }
+
+    cmd_list_->Close();
+
+    return true;
 }
 
 LRESULT CALLBACK App::window_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
--- d3d12/HelloWorld/app.h
+++ d3d12/HelloWorld/app.h
@@ -1,7 +1,11 @@
 #pragma once
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 #include <Windows.h>
 #include <cstdint>
+#include <d3d12.h>
+#include <dxgi1_4.h>
 
 class App {
   public:
@@ -15,11 +19,29 @@ class App {
     bool init_window();
     void term_window();
     void main_loop();
+    bool init_d3d();
+    void term_d3d();
+    void render();
+    void wait_gpu();
+    void present(uint32_t const interval);
 
     static LRESULT CALLBACK window_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+    static const uint32_t FRAME_COUNT = 2; // Number of frame buffers
 
     HINSTANCE hinst_;
     HWND hwnd_;
     uint32_t width_;
     uint32_t height_;
+    ID3D12Device *device_;        // Physical device
+    ID3D12CommandQueue *queue_;   // Command queue to submit draw commands to GPU
+    IDXGISwapChain3 *swap_chain_; // Swap frame buffers (double buffer)
+    ID3D12Resource *color_buffer_[FRAME_COUNT];
+    ID3D12CommandAllocator *cmd_alloc_[FRAME_COUNT];
+    ID3D12GraphicsCommandList *cmd_list_;
+    ID3D12DescriptorHeap *heap_rtv_;
+    ID3D12Fence *fence_; // Fence between CPU and GPU
+    HANDLE fence_event_;
+    uint64_t fence_counter_[FRAME_COUNT];
+    uint32_t frame_index_;
+    D3D12_CPU_DESCRIPTOR_HANDLE handle_rtv_[FRAME_COUNT];
 };
