2592f8f3eb78d44d14644dc97051bbd2e275d89a
rhysd <lin90162@yahoo.co.jp>
2025-07-05 23:25:03 +0900

    d3d12: add first vertex and pixel shaders

---
+++ d3d12/HelloWorld/vs.hlsl
@@ -0,0 +1,29 @@
+struct VSInput {
+    float3 Position : POSITION;
+    float4 Color : COLOR;
+};
+
+struct VSOutput {
+    float4 Position : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+cbuffer Transform : register(b0) {
+    float4x4 World : packoffset(c0);
+    float4x4 View : packoffset(c4);
+    float4x4 Proj : packoffset(c8);
+}
+
+VSOutput main( VSInput input : POSITION ) {
+    VSOutput output;
+
+    float4 local = float4(input.Position, 1.0f);
+    float4 world = mul(World, local);
+    float4 view = mul(View, world);
+    float4 proj = mul(Proj, view);
+
+    output.Position = proj;
+    output.Color = input.Color;
+
+    return output;
+}
