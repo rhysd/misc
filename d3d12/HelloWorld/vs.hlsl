333871ec4fe5bf470265577938120c155189696b
rhysd <lin90162@yahoo.co.jp>
2025-07-06 21:32:51 +0900

    d3d12: fix semantics of vertex shader input

--- d3d12/HelloWorld/vs.hlsl
+++ d3d12/HelloWorld/vs.hlsl
@@ -14,7 +14,7 @@ cbuffer Transform : register(b0) {
     float4x4 Proj : packoffset(c8);
 }
 
-VSOutput main( VSInput input : POSITION ) {
+VSOutput main(VSInput input) {
     VSOutput output;
 
     float4 local = float4(input.Position, 1.0f);
