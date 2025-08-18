fad5c47e98bbf20bd18a70b00d4bbd9dcf6f5ab6
rhysd <lin90162@yahoo.co.jp>
2025-08-18 22:19:51 +0900

    webgl: encapsulate program and its attributes

--- webgl-tutorial/script.ts
+++ webgl-tutorial/script.ts
@@ -44,22 +44,6 @@
         }
     }
 
-    function createProgram(vs: WebGLShader, fs: WebGLShader): WebGLProgram {
-        const program = gl.createProgram();
-
-        gl.attachShader(program, vs);
-        gl.attachShader(program, fs);
-
-        gl.linkProgram(program);
-
-        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            gl.useProgram(program);
-            return program;
-        } else {
-            throw new Error(`Could not craete program: ${gl.getProgramInfoLog(program)}`);
-        }
-    }
-
     function createVertexBuffer(data: number[]): WebGLBuffer {
         const vbo = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
@@ -74,12 +58,6 @@
         stride: number;
     }
 
-    function createAttribute(name: string, data: number[], stride: number, program: WebGLProgram): Attribute {
-        const loc = gl.getAttribLocation(program, name);
-        const vbo = createVertexBuffer(data);
-        return { loc, vbo, stride };
-    }
-
     interface RenderObject {
         attrs: Attribute[];
         ibo: WebGLBuffer;
@@ -91,19 +69,6 @@
         colors: number[];
         normals: number[];
         indices: number[];
-    }
-
-    function createObject(prog: WebGLProgram, data: ObjectData): RenderObject {
-        const { positions, colors, normals, indices } = data;
-        return {
-            attrs: [
-                createAttribute('position', positions, 3, prog),
-                createAttribute('color', colors, 4, prog),
-                createAttribute('normal', normals, 3, prog),
-            ],
-            ibo: createIndexBuffer(indices),
-            lenIndices: indices.length,
-        };
     }
 
     function createIndexBuffer(data: number[]): WebGLBuffer {
@@ -122,6 +87,88 @@
             gl.vertexAttribPointer(loc, stride, gl.FLOAT, false, 0, 0);
         }
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, object.ibo);
+    }
+
+    interface BoundAttribute {
+        loc: number;
+        vbo: WebGLBuffer;
+        stride: number;
+    }
+
+    interface AttributeDef {
+        name: string;
+        dataName: keyof ObjectData;
+        stride: number;
+    }
+
+    class Program {
+        private prog: WebGLProgram;
+        private attrLocs: Record<string, number>;
+        private uniforms: Record<string, WebGLUniformLocation>;
+        private attrDefs: AttributeDef[];
+
+        constructor(vs: WebGLShader, fs: WebGLShader) {
+            const p = gl.createProgram();
+
+            gl.attachShader(p, vs);
+            gl.attachShader(p, fs);
+            gl.linkProgram(p);
+            if (!gl.getProgramParameter(p, gl.LINK_STATUS)) {
+                throw new Error(`Could not craete program: ${gl.getProgramInfoLog(p)}`);
+            }
+            gl.useProgram(p);
+
+            this.prog = p;
+            this.attrLocs = {};
+            this.uniforms = {};
+            this.attrDefs = [];
+        }
+
+        use(): void {
+            gl.useProgram(this.prog);
+        }
+
+        defineAttribute(name: string, dataName: keyof ObjectData, stride: number): void {
+            this.attrDefs.push({ name, dataName, stride });
+        }
+
+        getAttributeLocation(name: string): number {
+            let loc = this.attrLocs[name];
+            if (loc === undefined) {
+                loc = gl.getAttribLocation(this.prog, name);
+                this.attrLocs[name] = loc;
+            }
+            return loc;
+        }
+
+        createAttribute(name: string, data: number[], stride: number): BoundAttribute {
+            const loc = this.getAttributeLocation(name);
+            const vbo = createVertexBuffer(data);
+            return { loc, vbo, stride };
+        }
+
+        createObject(data: ObjectData): RenderObject {
+            const attrs = this.attrDefs.map(def => {
+                return this.createAttribute(def.name, data[def.dataName], def.stride);
+            });
+            const ibo = createIndexBuffer(data.indices);
+            const lenIndices = data.indices.length;
+            return { attrs, ibo, lenIndices };
+        }
+
+        declareUniforms(...names: string[]): void {
+            for (const name of names) {
+                this.uniforms[name] = gl.getUniformLocation(this.prog, name)!;
+            }
+        }
+
+        uniform(name: string): WebGLUniformLocation {
+            const loc = this.uniforms[name];
+            if (loc === undefined) {
+                throw new Error(`Unknown uniform variable: ${name}`);
+            }
+            return loc;
+        }
     }
 
     function hsva(h: number, s: number, v: number, a: number): Color {
@@ -214,6 +261,92 @@
         return { positions, normals, colors, indices };
     }
 
+    function rect(size: number, color: Color): ObjectData {
+        const half = size / 2;
+        // prettier-ignore
+        const positions = [
+            -half,  half, 0.0,
+             half,  half, 0.0,
+            -half, -half, 0.0,
+             half, -half, 0.0,
+        ];
+        // prettier-ignore
+        const normals = [
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+        ];
+        const colors = [...color, ...color, ...color, ...color];
+        // prettier-ignore
+        const texCoords = [
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0
+        ];
+        // prettier-ignore
+        const indices = [
+            0, 2, 1,
+            2, 3, 1,
+        ];
+        // Use `tmp` variable to ignore `texCoords` for now
+        const tmp = { positions, normals, colors, texCoords, indices };
+        return tmp;
+    }
+
+    class Camera {
+        pos: Vec3;
+        upDir: Vec3;
+        q: Float32Array;
+
+        constructor(canvas: HTMLCanvasElement) {
+            this.pos = [0, 0, 0];
+            this.upDir = [0, 0, 0];
+            this.q = q.identity(q.create());
+
+            canvas.addEventListener(
+                'mousemove',
+                event => {
+                    const w = canvas.width;
+                    const h = canvas.height;
+                    const x = event.clientX - canvas.offsetLeft - w / 2;
+                    const y = event.clientY - canvas.offsetTop - h / 2;
+                    const len = Math.sqrt(x * x + y * y);
+
+                    // Normalize position
+                    const normX = x / len;
+                    const normY = y / len;
+
+                    // Use distance from the center of canvas to calculate the angle
+                    const diag = Math.sqrt(w * w + h * h);
+                    const rad = 2 * Math.PI * (len / diag);
+
+                    // Calculate quaternion to rotate the model
+                    q.rotate(rad, [normY, normX, 0], this.q);
+                },
+                { passive: true },
+            );
+
+            canvas.addEventListener(
+                'mouseleave',
+                () => {
+                    q.identity(this.q);
+                },
+                { passive: true },
+            );
+        }
+
+        position(): Vec3 {
+            q.toVecIII([0, 5, 5], this.q, this.pos);
+            return this.pos;
+        }
+
+        up(): Vec3 {
+            q.toVecIII([0, 1, -1], this.q, this.upDir);
+            return this.upDir;
+        }
+    }
     async function main(): Promise<void> {
         const [vs, fs] = await Promise.all([loadShader('shader.vert'), loadShader('shader.frag')]);
 
@@ -227,10 +360,16 @@
         gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE);
         gl.blendEquationSeparate(gl.FUNC_ADD, gl.FUNC_ADD);
 
-        const prog = createProgram(vs, fs);
+        const prog = new Program(vs, fs);
+        prog.defineAttribute('position', 'positions', 3);
+        prog.defineAttribute('color', 'colors', 4);
+        prog.defineAttribute('normal', 'normals', 3);
+        prog.declareUniforms('mMat', 'vpMat', 'invMat', 'lightDirection', 'eyePosition');
+        prog.use();
 
-        const torusObject = createObject(prog, torus(64, 64, 0.1, 0.4));
-        const sphereObject = createObject(prog, sphere(64, 64, 0.25));
+        const torusObject = prog.createObject(torus(64, 64, 0.1, 0.4));
+        const sphereObject = prog.createObject(sphere(64, 64, 0.25));
+        const rectObject = prog.createObject(rect(2, [0.5, 0.5, 0.5, 1]));
 
         const vMat = m.identity(m.create());
         const pMat = m.identity(m.create());
@@ -244,42 +383,8 @@
             pMat,
         );
 
-        const lightDirection: [number, number, number] = [-0.577, 0.577, 0.577];
-        const cameraPos: [number, number, number] = [0, 5, 5];
-        const cameraUp: [number, number, number] = [0, 1, -1];
-        const qCamera = q.identity(q.create());
-
-        canvas.addEventListener(
-            'mousemove',
-            event => {
-                const w = canvas.width;
-                const h = canvas.height;
-                const x = event.clientX - canvas.offsetLeft - w / 2;
-                const y = event.clientY - canvas.offsetTop - h / 2;
-                const len = Math.sqrt(x * x + y * y);
-
-                // Normalize position
-                const normX = x / len;
-                const normY = y / len;
-
-                // Use distance from the center of canvas to calculate the angle
-                const diag = Math.sqrt(w * w + h * h);
-                const rad = 2 * Math.PI * (len / diag);
-
-                // Calculate quaternion to rotate the model
-                q.rotate(rad, [normY, normX, 0], qCamera);
-            },
-            { passive: true },
-        );
-
-        const uniforms = ['mMat', 'vpMat', 'invMat', 'lightDirection', 'eyePosition'].reduce(
-            (acc, name) => {
-                acc[name] = gl.getUniformLocation(prog, name)!;
-                return acc;
-            },
-            {} as Record<string, WebGLUniformLocation>,
-        );
-
+        const camera = new Camera(canvas);
+        const lightDirection: Vec3 = [-0.577, 0.577, 0.577];
         const mMat = m.create();
         const invMat = m.create();
 
@@ -291,49 +396,47 @@
             const rad = ((count % 720) * Math.PI) / 360;
             const upDown = Math.sin(rad) * 0.25;
 
-            function drawElementsWithOutline(lenIndices: number): void {
-                gl.cullFace(gl.BACK);
-                gl.drawElements(gl.TRIANGLES, lenIndices, gl.UNSIGNED_SHORT, 0);
-            }
-
-            q.toVecIII([0, 5, 5], qCamera, cameraPos);
-            q.toVecIII([0, 1, -1], qCamera, cameraUp);
-            m.lookAt(cameraPos, /* Camera center */ [0, 0, 0], cameraUp, vMat);
+            const cameraPos = camera.position();
+            m.lookAt(cameraPos, /* Camera center */ [0, 0, 0], camera.up(), vMat);
             m.multiply(pMat, vMat, vpMat);
-            gl.uniformMatrix4fv(uniforms.vpMat, /* transpose */ false, vpMat);
+            gl.uniformMatrix4fv(prog.uniform('vpMat'), /* transpose */ false, vpMat);
 
-            gl.uniform3fv(uniforms.lightDirection, lightDirection);
-            gl.uniform3fv(uniforms.eyePosition, cameraPos);
+            gl.uniform3fv(prog.uniform('lightDirection'), lightDirection);
+            gl.uniform3fv(prog.uniform('eyePosition'), cameraPos);
+
+            function drawObject(obj: RenderObject, m: Float32Array, inv: Float32Array): void {
+                bindObjectBuffers(obj);
+                gl.uniformMatrix4fv(prog.uniform('mMat'), /* transpose */ false, m);
+                gl.uniformMatrix4fv(prog.uniform('invMat'), /* transpose */ false, inv);
+                gl.drawElements(gl.TRIANGLES, obj.lenIndices, gl.UNSIGNED_SHORT, 0);
+            }
 
             // Render torus object
             {
-                bindObjectBuffers(torusObject);
-
                 m.identity(mMat);
                 m.rotate(mMat, rad, /* axis */ [0, 1, 0], mMat);
                 m.translate(mMat, [0, 0.75 + upDown, 0], mMat);
                 m.rotate(mMat, Math.PI * 0.5, [1, 0, 0], mMat);
                 m.inverse(mMat, invMat);
-
-                gl.uniformMatrix4fv(uniforms.mMat, /* transpose */ false, mMat);
-                gl.uniformMatrix4fv(uniforms.invMat, /* transpose */ false, invMat);
-
-                drawElementsWithOutline(torusObject.lenIndices);
+                drawObject(torusObject, mMat, invMat);
             }
 
             // Render sphere object
             {
-                bindObjectBuffers(sphereObject);
-
                 m.identity(mMat);
                 m.rotate(mMat, -rad, /* axis */ [0, 1, 0], mMat);
                 m.translate(mMat, [0, 0.75, 1], mMat);
                 m.inverse(mMat, invMat);
+                drawObject(sphereObject, mMat, invMat);
+            }
 
-                gl.uniformMatrix4fv(uniforms.mMat, /* transpose */ false, mMat);
-                gl.uniformMatrix4fv(uniforms.invMat, /* transpose */ false, invMat);
-
-                drawElementsWithOutline(sphereObject.lenIndices);
+            // Render the board
+            {
+                m.identity(mMat);
+                m.rotate(mMat, Math.PI * 1.5, [1.0, 0.0, 0.0], mMat);
+                m.scale(mMat, [2.0, 2.0, 1.0], mMat);
+                m.inverse(mMat, invMat);
+                drawObject(rectObject, mMat, invMat);
             }
 
             // Actual re-rendering happens here
