1774af4525f5d3e0a7d059e9f5852d9a6a4e0e9d
rhysd <lin90162@yahoo.co.jp>
2025-12-30 20:30:19 +0900

    mhwsgac: add missing neutral element

--- mhwsgac/index.ts
+++ mhwsgac/index.ts
@@ -31,6 +31,8 @@ function elementNameToClass(name: string): string {
             return 'sleep';
         case '爆破':
             return 'explosion';
+        case '無':
+            return 'neutral';
         default:
             throw new Error(`Unexpected element name: ${name}`);
     }
