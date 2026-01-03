390eff76e761ce2ce106df1a49c1bb2fb5769396
rhysd <lin90162@yahoo.co.jp>
2026-01-03 18:21:08 +0900

    wgpu: clear wgpu surface in window

--- wgpu/2026/src/main.rs
+++ wgpu/2026/src/main.rs
@@ -1,22 +1,132 @@
+use std::iter;
+use std::sync::Arc;
 use winit::application::ApplicationHandler;
 use winit::event::{KeyEvent, WindowEvent};
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::keyboard::{KeyCode, PhysicalKey};
 use winit::window::{Window, WindowId};
 
-#[derive(Default)]
-pub struct App {
-    window: Option<Window>,
+struct Renderer {
+    surface: wgpu::Surface<'static>,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
+    window: Arc<Window>,
 }
 
-impl App {
-    fn resize(&mut self, _width: u32, _height: u32) {
-        let Some(_window) = &self.window else { return };
+impl Renderer {
+    async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+        let surface = instance.create_surface(window.clone())?;
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(), // LowPower is the default
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await?;
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                ..Default::default()
+            })
+            .await?;
+        let caps = surface.get_capabilities(&adapter);
+        let format = caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(caps.formats[0]);
+        let size = window.inner_size();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: size.width,
+            height: size.height,
+            present_mode: caps.present_modes[0],
+            alpha_mode: caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        Ok(Self {
+            surface,
+            adapter,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+            window,
+        })
+    }
+
+    fn resize(&mut self, width: u32, height: u32) {
+        if width == 0 || height == 0 {
+            return;
+        }
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+        self.is_surface_configured = true;
+    }
+
+    fn update(&mut self) {
         // todo
     }
 
-    fn render(&mut self) {
-        // todo
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        self.window.request_redraw();
+
+        if !self.is_surface_configured {
+            return Ok(());
+        }
+
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+
+        {
+            let attachment = wgpu::RenderPassColorAttachment {
+                view: &view,
+                depth_slice: None,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            };
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[Some(attachment)],
+                ..Default::default()
+            });
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+struct App {
+    renderer: Option<Renderer>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self { renderer: None }
     }
 }
 
@@ -25,14 +135,19 @@ impl ApplicationHandler for App {
         let window = event_loop
             .create_window(Window::default_attributes())
             .unwrap();
-        self.window = Some(window);
+        let renderer = pollster::block_on(Renderer::new(Arc::new(window))).unwrap();
+        self.renderer = Some(renderer);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        let renderer = self.renderer.as_mut().unwrap();
+
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::Resized(size) => self.resize(size.width, size.height),
-            WindowEvent::RedrawRequested => self.render(),
+            WindowEvent::Resized(size) => renderer.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                renderer.render(); // TODO: Handle error
+            }
             WindowEvent::KeyboardInput {
                 event:
                     KeyEvent {
@@ -50,7 +165,9 @@ impl ApplicationHandler for App {
     }
 }
 
-fn main() {
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.run_app(&mut App::default()).unwrap();
+fn main() -> anyhow::Result<()> {
+    let event_loop = EventLoop::new()?;
+    let mut app = App::new();
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
