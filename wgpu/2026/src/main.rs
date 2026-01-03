5c0d8740a4fea1bdb6c10478e61a72eea1ab7d07
rhysd <lin90162@yahoo.co.jp>
2026-01-03 14:55:47 +0900

    wgpu: windowing

--- wgpu/2026/src/main.rs
+++ wgpu/2026/src/main.rs
@@ -1,3 +1,56 @@
+use winit::application::ApplicationHandler;
+use winit::event::{KeyEvent, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::keyboard::{KeyCode, PhysicalKey};
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+pub struct App {
+    window: Option<Window>,
+}
+
+impl App {
+    fn resize(&mut self, _width: u32, _height: u32) {
+        let Some(_window) = &self.window else { return };
+        // todo
+    }
+
+    fn render(&mut self) {
+        // todo
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(Window::default_attributes())
+            .unwrap();
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => self.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => self.render(),
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state,
+                        ..
+                    },
+                ..
+            } => match code {
+                KeyCode::Escape if state.is_pressed() => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.run_app(&mut App::default()).unwrap();
 }
